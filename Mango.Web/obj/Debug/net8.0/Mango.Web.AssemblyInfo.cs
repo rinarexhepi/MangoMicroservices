a8ae3af7f75c890e4e9458c42793a4ffdc8a734c
rinarexhepi <rinnaarexhepi@gmail.com>
2024-08-15 20:40:07 +0200

    homepage

--- Mango.Web/obj/Debug/net8.0/Mango.Web.AssemblyInfo.cs
+++ Mango.Web/obj/Debug/net8.0/Mango.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mango.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8242b6538c0d2231f6753ce531f137bcb2be3a04")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7eae76fa65d2493d8ca3b6d3e8ff99ad94e9e9d3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mango.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mango.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
